030f6ea16ac89f9ba1275177e2b933e687849336
devOpsYeole <akshayyeole888@gmail.com>
2025-11-13 17:44:48 +0530

    Initial commit

---
+++ src/main/java/calcy/yeole/SumDiffCalcy.java
@@ -0,0 +1,20 @@
+package calcy.yeole;
+
+public class SumDiffCalcy {
+
+    int sum(int a, int b) {
+        return a + b;
+    }
+
+    int diff(int a, int b) {
+
+        return a - b;
+    }
+
+    public static void main(String[] args) {
+        SumDiffCalcy calci = new SumDiffCalcy();
+        System.out.println("sum is :" + calci.sum(20, 10));
+        System.out.println("diff is :" + calci.diff(20, 10));
+    }
+}
+
