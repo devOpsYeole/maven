fc6f5587a7eaac144ed43748a9b464df0bee4f1a
devOpsYeole <akshayyeole888@gmail.com>
2025-11-13 19:05:44 +0530

    multiplication

--- src/main/java/calcy/yeole/SumDiffCalcy.java
+++ src/main/java/calcy/yeole/SumDiffCalcy.java
@@ -11,10 +11,16 @@ public class SumDiffCalcy {
         return a - b;
     }
 
+    int mul(int a, int b) {
+
+        return a * b;
+    }
+
     public static void main(String[] args) {
         SumDiffCalcy calci = new SumDiffCalcy();
         System.out.println("sum is :" + calci.sum(20, 10));
         System.out.println("diff is :" + calci.diff(20, 10));
+        System.out.println("mul is :" + calci.mul(20, 10));
     }
 }
 
