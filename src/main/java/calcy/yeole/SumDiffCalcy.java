78c5908b1686898314ac4b1a839a92f2ba5e52ca
devOpsYeole <akshayyeole888@gmail.com>
2025-11-13 19:17:08 +0530

    division added

--- src/main/java/calcy/yeole/SumDiffCalcy.java
+++ src/main/java/calcy/yeole/SumDiffCalcy.java
@@ -16,11 +16,18 @@ public class SumDiffCalcy {
         return a * b;
     }
 
+    int div(int a, int b) {
+
+        return a / b;
+    }
+
     public static void main(String[] args) {
         SumDiffCalcy calci = new SumDiffCalcy();
         System.out.println("sum is :" + calci.sum(20, 10));
         System.out.println("diff is :" + calci.diff(20, 10));
         System.out.println("mul is :" + calci.mul(20, 10));
+        System.out.println("div is :" + calci.div(20, 10));
+
     }
 }
 
